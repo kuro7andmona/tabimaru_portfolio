e6e71943807e3eb7062a60b36f74339c2738b79e
kuro7andmona <129634798+kuro7andmona@users.noreply.github.com>
2023-07-08 13:09:04 +0000

    routes create

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,29 @@ Rails.application.routes.draw do
 
  scope module: :public do
 
+   root to: 'homes#top'
+
+   get 'about' => 'homes#about'
+
+   get 'users' => 'users#index'
+   get 'users/new' => 'users#new'
+   get 'users/mypage' => 'users#show'
+   get 'users/edit' => 'users#edit'
+   patch 'users/update' => 'users#update'
+
+   resources :bookmark, only[:index, :destoroy]
+
+  end
 #管理者用
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+namespace :admin do
+
+  get '/' => 'homes#top'
+
+  resources :users, only: [:index, :destroy]
+  resources :comment, only: [:index, :destroy]
+end
 end
