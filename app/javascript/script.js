779536a808cbee52a6d891970c486f8d8b170958
kuro7andmona <129634798+kuro7andmona@users.noreply.github.com>
2023-07-27 13:16:38 +0000

    fix summer.jpg

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -7,15 +7,3 @@ $(function() {
   });
 });
 
-function sliderStart() {
-
-    const slide = document.getElementById('slide_wrap');      //スライダー親
-    const slideItem = slide.querySelectorAll('.slide_item');   //スライド要素
-    const totalNum = slideItem.length - 1;                     // スライドの枚数を取得
-    const FadeTime = 2000;                                     //フェードインの時間
-    const IntarvalTime = 5000;                                 //クロスフェードさせるまでの間隔
-    let actNum = 0;                                            //現在アクティブな番号
-    let nowSlide;                                              //現在表示中のスライド
-    let NextSlide;
-
-}
